40d577c0a0d879b5f5893f187cd59f7f004ddcab
m09 <mog@crydee.eu>
2017-10-18 14:50:38 +0200

    improve config and add some scripts to manage the cluster, config & co.

---
+++ source.sh
@@ -0,0 +1,38 @@
+dlc_home="$(pwd)"
+alias dlc-check='kubectl --namespace=dlc-jupyterhub get pod'
+alias dlc-resize=""
+
+function dlc-resize {
+    gcloud container clusters resize deeplearning-codelab \
+           --zone europe-west1-b \
+           --size $1
+}
+
+function dlc-reload-config {
+    helm upgrade dlc-jupyterhub jupyterhub/jupyterhub \
+         --version=v0.4 \
+         -f "$dlc_home/config.yaml"
+}
+
+function _dlc-make-config {
+    sed "s/%%TAG%%/$1/g" < "$dlc_home/config.yaml.template"
+}
+
+function dlc-upload {
+    # https://zero-to-jupyterhub.readthedocs.io/en/latest/user-experience.html#build-a-custom-image-with-repo2docker
+    local commit_id="$(cd "$dlc_home" ; git rev-parse HEAD)"
+    local short_commit_id="${commit_id:0:6}"
+    echo "Using $short_commit_id as tag"
+    echo "Building Docker image with repo2docker"
+    jupyter-repo2docker \
+        . \
+        --image=gcr.io/deeplearning-codelab-183309/workspace:$short_commit_id \
+        --no-run
+    echo "Uploading Docker image on gcloud"
+    gcloud docker -- \
+           push gcr.io/deeplearning-codelab-183309/workspace:$short_commit_id
+    echo "Updating config file"
+    _dlc-make-config $short_commit_id > "$dlc_home/config.yaml"
+    echo "Upload new config file"
+    dlc-reload-config
+}
