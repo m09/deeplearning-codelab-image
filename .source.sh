8767233ced8f62e7b2a49d06903d8265c5cfcb28
m09 <mog@crydee.eu>
2017-10-18 15:55:34 +0200

    improve source.sh

--- .source.sh
+++ .source.sh
@@ -15,32 +15,38 @@ function dlc-reload-config {
          -f "$dlc_home/.config.yaml"
 }
 
-function _dlc-make-config {
+function _dlc-get-tag {
+    local commit_id="$(cd "$dlc_home" ; git rev-parse HEAD)"
+    echo "${commit_id:0:6}"
+}
+
+function dlc-make-config {
+    local tag="$(_dlc-get-tag)"
     local github_secret="$(< .github-secret.key)"
     local hub_secret="$(< .hub-secret.key)"
     local proxy_secret="$(< .proxy-secret.key)"
-    sed -e "s/%%TAG%%/$1/g" \
+    sed -e "s/%%TAG%%/$tag/g" \
         -e "s/%%GITHUBSECRET%%/$github_secret/g" \
         -e "s/%%HUBSECRET%%/$hub_secret/g" \
         -e "s/%%PROXYSECRET%%/$proxy_secret/g" \
-        < "$dlc_home/.config.yaml.template"
+        < "$dlc_home/.config.yaml.template" \
+        > "$dlc_home/.config.yaml"
 }
 
 function dlc-upload {
     # https://zero-to-jupyterhub.readthedocs.io/en/latest/user-experience.html#build-a-custom-image-with-repo2docker
-    local commit_id="$(cd "$dlc_home" ; git rev-parse HEAD)"
-    local short_commit_id="${commit_id:0:6}"
-    echo "Using $short_commit_id as tag"
+    local tag="$(_dlc-get-tag)"
+    echo "Using $tag as tag"
     echo "Building Docker image with repo2docker"
     jupyter-repo2docker \
         "$dlc_home" \
-        --image=gcr.io/deeplearning-codelab-183309/workspace:$short_commit_id \
+        --image=gcr.io/deeplearning-codelab-183309/workspace:$tag \
         --no-run
     echo "Uploading Docker image on gcloud"
     gcloud docker -- \
-           push gcr.io/deeplearning-codelab-183309/workspace:$short_commit_id
+           push gcr.io/deeplearning-codelab-183309/workspace:$tag
     echo "Updating config file"
-    _dlc-make-config $short_commit_id > "$dlc_home/.config.yaml"
+    dlc-make-config
     echo "Upload new config file"
     dlc-reload-config
 }
